24b5cc13ecea4344167e59df092111655cc3a85c
uxb2b-Amy <puma_rong@hotmail.com>
2020-07-20 19:44:01 +0800

    統計報表>發票明細查詢

---
+++ eIVOGo/Views/InvoiceQuery/InvoiceQuery_DownloadCSV.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace eIVOGo.Views.InvoiceQuery
+{
+    public partial class InvoiceQuery_DownloadCSV : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
