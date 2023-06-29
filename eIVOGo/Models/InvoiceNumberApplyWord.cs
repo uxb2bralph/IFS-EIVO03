b59636c89a5a08ae0b93386a2cd1b69410eb3e4a
iris <ripsea0505@hotmail.com>
2023-06-29 14:41:46 +0800

    [2023.06.29]:新增[電子發票號碼申請作業電子化]-網頁申請/Word套表及申請查詢

---
+++ eIVOGo/Models/InvoiceNumberApplyWord.cs
@@ -0,0 +1,264 @@
+﻿using ApplicationResource;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using DocumentFormat.OpenXml.Wordprocessing;
+using eIVOGo.Helper.DataQuery;
+using eIVOGo.Properties;
+using Microsoft.Ajax.Utilities;
+using PdfSharp.Pdf.Content.Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eIVOGo.Models.ViewModel
+{
+
+    public class InvoiceNumberApplyWord
+    {
+        public static string UnCheckedBox = "□";
+        public static string CheckedBox = "■";
+        public static string UnCheckedRadioButton = "○";
+        public static string CheckedRadioButton = "●";
+
+        //ta00
+        //t:text(r:radiobutton c:checkbox)
+        //a:受理機關(b:申請項目 c:檢核文件 d:資料填寫)
+        //0:區塊
+        //0:流水號
+
+        #region 受理機關
+
+        public string ta00 { get; set; } = string.Empty; //MOFBranch
+        public string ta01 { get; set; } = string.Empty; //TaxBranch
+        #endregion
+
+        #region 申請項目-[申請整合服務平台]
+        
+        public string cb10 { get; set; } = UnCheckedBox; //ApplyUse=true
+        public string rb10 { get; set; } = UnCheckedRadioButton; //ApplyUse_NeedNumber=true
+        public string rb11 { get; set; } = UnCheckedRadioButton; //ApplyUse_NeedNumber=false
+        #endregion
+
+        #region 申請項目-[電子發票字軌號碼]及[配號方式]
+        //一般稅額計算
+        public string cb20 { get; set; } = UnCheckedBox; //ApplyNumberNomal=true
+        public string rb20 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=apply
+        public string rb21 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=increase
+        public string rb22 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=deduct
+        public string rb23 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=zero
+        public string tb20 { get; set; } = string.Empty; //ApplyNumberQuantity
+        //特種稅額計算
+        public string cb21 { get; set; } = UnCheckedBox; //ApplyNumberNomal=false
+        public string rb24 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=apply
+        public string rb25 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=increase
+        public string rb26 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=deduct
+        public string rb27 { get; set; } = UnCheckedRadioButton; //ApplyNumberType=zero
+        public string tb21 { get; set; } = string.Empty; //ApplyNumberQuantity
+        public string cb30 { get; set; } = UnCheckedBox; //IsGetNumberByYear=false
+        public string cb31 { get; set; } = UnCheckedBox; //IsGetNumberByYear=true
+        public string rb30 { get; set; } = UnCheckedRadioButton; //GetNumberByYearReason=1
+        public string rb31 { get; set; } = UnCheckedRadioButton; //GetNumberByYearReason=2
+        public string rb32 { get; set; } = UnCheckedRadioButton; //GetNumberByYearReason=3
+        public string rb33 { get; set; } = UnCheckedRadioButton; //GetNumberByYearReason=4
+        public string rb34 { get; set; } = UnCheckedRadioButton; //GetNumberByYearReason=5
+        public string tb30 { get; set; } = string.Empty; //GetNumberByYearReasonBusiness
+        #endregion
+
+        #region 申請項目-停用整合服務平台功能/委任加值服務中心申請
+        public string cb40 { get; set; } = UnCheckedBox;
+        public string cb50 { get; set; } = UnCheckedBox;
+
+        #endregion
+
+        #region 僅開立B2B或B2G電子發票營業人文件
+        public string cc00 { get; set; } = UnCheckedBox; //b2bRequiredDoc1
+        public string cc01 { get; set; } = UnCheckedBox;
+        public string cc02 { get; set; } = UnCheckedBox;
+        public string cc03 { get; set; } = UnCheckedBox;
+        public string tc10 { get; set; } = UnCheckedBox; //b2bTransferDoc1TurnkeyToken
+        public string cc11 { get; set; } = UnCheckedBox; //b2bTransferDoc1=true
+        public string cc12 { get; set; } = UnCheckedBox; //b2bTransferDoc1=false
+        public string tc13 { get; set; } = UnCheckedBox; //b2bTransferDoc2BusinessID
+        public string tc14 { get; set; } = UnCheckedBox; //b2bTransferDoc2BusinessTaxID
+        public string cc15 { get; set; } = UnCheckedBox; //b2bTransferDoc2=true
+        public string cc16 { get; set; } = UnCheckedBox; //b2bTransferDoc2=false
+        public string cc17 { get; set; } = UnCheckedBox;//b2bTransferDoc3=true
+        public string cc18 { get; set; } = UnCheckedBox;//b2bTransferDoc3=false
+        public string cc20 { get; set; } = UnCheckedBox;//b2bDoc1=true
+        public string cc21 { get; set; } = UnCheckedBox;//b2bDoc1=false
+        public string cc22 { get; set; } = UnCheckedBox;//b2bDoc2=true
+        public string cc23 { get; set; } = UnCheckedBox;//b2bDoc2=false
+        public string cc24 { get; set; } = UnCheckedBox;//b2bDoc3=true
+        public string cc25 { get; set; } = UnCheckedBox;//b2bDoc3=false
+        public string cc26 { get; set; } = UnCheckedBox;//b2bDoc4=true
+        public string cc27 { get; set; } = UnCheckedBox;//b2bDoc4=false
+        public string cc28 { get; set; } = UnCheckedBox;//b2bDoc5=true
+        public string cc29 { get; set; } = UnCheckedBox;//b2bDoc5=false
+
+        #endregion
+
+        #region 同時開立b2c及b2c電子發票營業人文件
+        public string cc30 { get; set; } = UnCheckedBox;//b2cRequiredDoc1
+        public string cc31 { get; set; } = UnCheckedBox;//b2cRequiredDoc2
+        public string cc32 { get; set; } = UnCheckedBox;//b2cRequiredDoc3
+        public string cc33 { get; set; } = UnCheckedBox;//b2cRequiredDoc4
+        public string cc34 { get; set; } = UnCheckedBox;//b2cRequiredDoc5
+        public string tc40 { get; set; } = UnCheckedBox;//b2cTransferDoc1TurnkeyToken
+        public string cc40 { get; set; } = UnCheckedBox;//b2cTransferDoc1=true
+        public string cc41 { get; set; } = UnCheckedBox;//b2cTransferDoc1=false
+        public string tc41 { get; set; } = UnCheckedBox;//b2cTransferDoc2BusinessID
+        public string tc42 { get; set; } = UnCheckedBox;//b2cTransferDoc2BusinessTaxID
+        public string cc42 { get; set; } = UnCheckedBox;//b2cTransferDoc2=true
+        public string cc43 { get; set; } = UnCheckedBox;//b2cTransferDoc2=false
+
+
+        public string rc64 { get; set; } = UnCheckedRadioButton;//b2cDoc1Type=1
+        public string rc65 { get; set; } = UnCheckedRadioButton;//b2cDoc1Type=2
+        public string rc66 { get; set; } = UnCheckedRadioButton;//b2cDoc1Type=3
+        public string tc67 { get; set; } = string.Empty;//b2cDoc1Other
+        public string cc50 { get; set; } = UnCheckedBox;//b2cDoc1=true
+        public string cc51 { get; set; } = UnCheckedBox;//b2cDoc1=false
+        public string cc52 { get; set; } = UnCheckedBox;//b2cDoc2=true
+        public string cc53 { get; set; } = UnCheckedBox;//b2cDoc2=false
+        public string cc54 { get; set; } = UnCheckedBox;//b2cDoc3=true
+        public string cc55 { get; set; } = UnCheckedBox;//b2cDoc3=false
+        public string cc56 { get; set; } = UnCheckedBox;//b2cDoc4=true
+        public string cc57 { get; set; } = UnCheckedBox;//b2cDoc4=false
+        public string cc58 { get; set; } = UnCheckedBox;//b2cDoc5=true
+        public string cc59 { get; set; } = UnCheckedBox;//b2cDoc5=false
+        public string cc60 { get; set; } = UnCheckedBox;//b2cDoc6=true
+        public string cc61 { get; set; } = UnCheckedBox;//b2cDoc6=false
+        public string cc62 { get; set; } = UnCheckedBox;//b2cDoc7=true
+        public string cc63 { get; set; } = UnCheckedBox;//b2cDoc7=false
+        #endregion
+
+        #region 申請人/聯絡方式
+        public string td00 { get; set; } = string.Empty; //BusinessName
+        public string td01 { get; set; } = string.Empty; //BusinessID
+        public string td02 { get; set; } = string.Empty; //BusinessTaxID
+        public string td03 { get; set; } = string.Empty; //BusinessAddr
+        public string td04 { get; set; } = string.Empty; //BusinessOwner 
+        public string td10 { get; set; } = string.Empty; //BusinessContactName
+        public string td11 { get; set; } = string.Empty; //BusinessMobile
+        public string td12 { get; set; } = string.Empty; //BusinessTelNo
+        public string td13 { get; set; } = string.Empty; //BusinessFaxNo
+        public string td14 { get; set; } = string.Empty; //BusinessContactAddr 
+        public string td15 { get; set; } = string.Empty; //BusinessEmail 
+
+        #endregion
+        #region 事務所
+        public string td20 { get; set; } = string.Empty; //AgentName
+        public string td21 { get; set; } = string.Empty; //AgentID 
+        public string td22 { get; set; } = string.Empty; //AgentContactName 
+        public string td23 { get; set; } = string.Empty; //AgentTelNo 
+        #endregion
+
+        #region 系統廠商
+        public string tf00 { get; set; }
+        public string tf01 { get; set; }
+        public string tf02 { get; set; }
+        public string tf03 { get; set; }
+        #endregion
+
+        #region 發票開立系統檢測作業
+        public string tf04 { get; set; }
+        public string tf05 { get; set; }
+        public string tf07 { get; set; }
+        public string tf06 { get; set; }
+        #endregion
+
+        #region 發票開立系統檢測項目
+        public string ce00 { get; set; } = UnCheckedBox; //SysChk11=true
+        public string ce01 { get; set; } = UnCheckedBox; //SysChk11=false
+        public string ce02 { get; set; } = UnCheckedBox; //SysChk12
+        public string ce03 { get; set; } = UnCheckedBox;//SysChk12
+        public string ce04 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce05 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce06 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce07 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce10 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce11 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce12 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce13 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce14 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce15 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce16 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce17 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce18 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce19 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce20 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce21 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce22 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce23 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce30 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce31 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce32 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce33 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce34 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce35 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce36 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce37 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce38 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce39 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce40 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce41 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce42 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce43 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce44 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce45 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce46 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce47 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce50 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce51 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce52 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce53 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce54 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce55 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce56 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce57 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce58 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce59 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce60 { get; set; } = UnCheckedBox;//SysChk13
+        public string ce61 { get; set; } = UnCheckedBox;//SysChk13
+        #endregion
+
+        #region 委任加值服務中心
+        public string td30 { get; set; } = "網際優勢股份有限公司"; //委任加值服務中心名稱
+        public string td31 { get; set; } = "02-23962339"; //委任加值服務中心聯絡電話
+        public string td32 { get; set; } = "70762419"; //委任加值服務中心統一編號 
+        public string td33 { get; set; } = "101700372"; //委任加值服務中心稅籍編號
+        public string td34 { get; set; } = "王瑞弟"; //委任加值服務中心負責人
+        public string td35 { get; set; } = string.Empty;//ApplyByAppointDateFrom
+        public string td36 { get; set; } = string.Empty;//ApplyByAppointDateTo
+        public string td37 { get; set; } = string.Empty;//ApplyByAppointDateFrom
+        public string td38 { get; set; } = string.Empty; //ApplyByAppointDateTo
+        public string td40 { get; set; } = string.Empty; //申請日期
+        #endregion
+
+        #region 感熱紙送驗報告
+        public string tg00 { get; set; }//TestBusinessName { get; set; }
+        public string tg01 { get; set; }//SubmitBusinessName { get; set; }
+        public string tg02 { get; set; }//ReportNo { get; set; }
+        public string tg03 { get; set; }//PaperName { get; set; }
+        public string tg04 { get; set; }//PaperNo { get; set; }
+        public string tg05 { get; set; }//PaperDate { get; set; }
+        #endregion
+
+
+
+    }
+
+    public enum ApplyNumberType
+    {
+        None = 0,
+        Apply = 1,
+        Increase =2,
+        Deduct =3,
+        Zero =4
+    }
+
+
+}
--- eIVOGo/Properties/AppSettings.cs
+++ eIVOGo/Properties/AppSettings.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ApplicationResource;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -27,13 +31,92 @@ namespace eIVOGo.Properties
         }
 
         public MonthlyBilling Billing { get; set; } = new MonthlyBilling { };
+        public InvoiceNumberApplySetting InvoiceNumberApplySetting = new InvoiceNumberApplySetting();
+        public IEnumerable<InvoiceNumberApplyWordSetting> InvoiceNumberApplyWordSetting { get; set; } = new List<InvoiceNumberApplyWordSetting>() { };
+    }
 
+    public class InvoiceNumberApplySetting
+    {
+        public string BasePath { get; set; }
+        public string BackupFolder { get; set; }
+        public string WordTemplateFolder { get; set; }
+        public string GetWordTemplateFolder(string wordFileName) {
+            return Path.Combine(AppSettings.AppRoot, WordTemplateFolder, wordFileName);
+        }
+
+        public string GetBackupPath() { return Path.Combine(BasePath, BackupFolder); }
+        public string JsonFileNameFormat { get; set; }
+        public string GetJsonFileName(string businessId) { return string.Format(JsonFileNameFormat, businessId); }
+        public string GetJsonFileFullPath(string businessId) { return string.Format("{0}"+ GetJsonFileName(businessId), BasePath); }
+        public string JsonFileNameFormatReg { get; set; }
+        public bool NotifyEnable { get; set; }
+        public string NotifyMailTo { get; set; }
+        public IEnumerable<InvoiceNumberApplySysSupplier> SysSupplier { get; set; }
+        public IEnumerable<InvoiceNumberApplyPaperTestSet> PaperTestSet { get; set; }
+    }
+
+    public class InvoiceNumberApplyWordSetting
+    {
+        public string ID { get; set; }
+        public string OutputName { get; set; }
+        public string TemplateFileName { get; set; }
+
+        public override string ToString()
+        {
+            return
+                "ID=" + ID +
+                "OutputName=" + OutputName +
+                "TemplateFileName=" + TemplateFileName;
+        }
+    }
+
+    public class InvoiceNumberApplyPaperTestSet
+    {
+        public string ID { get; set; }
+        public string TestBusinessName { get; set; }
+        public string SubmitBusinessName { get; set; }
+        public string ReportNo { get; set; }
+        public string PaperName { get; set; }
+        public string PaperNo { get; set; }
+
+        public string ReportDate { get; set; }
+        public override string ToString()
+        {
+            return
+                "ID=" + ID +
+                "TestBusinessName=" + TestBusinessName +
+                "SubmitBusinessName=" + SubmitBusinessName +
+                "ReportNo=" + ReportNo +
+                "PaperName=" + PaperName +
+                 "PaperNo=" + PaperNo +
+                 "ReportDate=" + ReportDate;
+        }
+    }
+
+    public class InvoiceNumberApplySysSupplier
+    {
+        public string ID { get; set; }
+        public string BusinessID { get; set; }
+        public string No { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+
+        public override string ToString()
+        {
+            return
+                "ID=" + ID +
+                "BusinessID=" + BusinessID +
+                "No=" + No +
+                "Name=" + Name +
+                "Version=" + Version;
+        }
     }
 
     public class MonthlyBilling
     {
         public int BillingDay { get; set; } = 5;
     }
+
 
 
 }
